ea202625064f699e6f100e0c45420ce5a1e1cf07
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2018-11-14 14:59:57 +0100

    Modify publicPath

--- app/App.js
+++ app/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 
 import MyRouter from './components/routes';
 import store from './components/store';
+import './public-path';
 
 ReactDOM.render(
   <Provider store={store}>
