24b764153d77f5dc36451e0a63dbbdc1f2e9a27a
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2018-11-15 17:32:45 +0100

    Serveur refresh

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
-var path = require('path');
-var express = require('express');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+const path = require('path');
+const express = require('express');
 require('dotenv').config();
-if (process.env.NODE_ENV == 'production') {
-    var app = express();
-    var staticPath = path.join(__dirname, './public');
-    app.use(express.static(staticPath));
-    // Allows you to set port in the project properties.
-    app.set('port', process.env.PORT || 3000);
-    app.listen(app.get('port'), function () {
-        const port = process.env.PORT
-        console.log(process.env.publicPath)
-        console.log('listening on port ' + port);
-    });
-} else {
-    new WebpackDevServer(webpack(config), {
-        contentBase: './public',
-        publicPath: config.output.publicPath,
-        hot: true,
-        historyApiFallback: true,
-        colors: true,
-        noInfo: true,
-        watchOptions: {
-            aggregateTimeout: 300,
-            poll: 1000
-        },
-    }).listen(3000, 'localhost', function (err, result) {
-        if (err) {
-            console.log(err);
-        }
-        console.log(process.env.NODE_ENV)
 
-        console.log('Listening at localhost:3000');
-    });
+if (process.env.NODE_ENV == 'production') {
+  const app = express();
+  const staticPath = path.join(__dirname, 'public');
+  app.use(express.static(staticPath));
+  app.all('*', (req, res) => res.sendFile(path.join(staticPath, 'index.html')));
+    // Allows you to set port in the project properties.
+  app.set('port', process.env.PORT || 3000);
+  app.listen(app.get('port'), () => {
+    const port = process.env.PORT;
+    console.log(process.env.publicPath);
+    console.log(`listening on port ${port}`);
+  });
+} else {
+  new WebpackDevServer(webpack(config), {
+    contentBase: './public',
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    colors: true,
+    noInfo: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  }).listen(3000, 'localhost', (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(process.env.NODE_ENV);
+
+    console.log('Listening at localhost:3000');
+  });
 }
